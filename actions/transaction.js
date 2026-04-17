143eb5cd0eda6ac10509fc8129fd5ef3bb9df4eb
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-17 15:39:27 +0530

    feat: add rate limiting using Arcjet

--- actions/transaction.js
+++ actions/transaction.js
@@ -23,32 +23,32 @@ export async function createTransaction(data) {
     // Get request data for ArcJet
     const req = await request();
 
-    // // Check rate limit
-    // const decision = await aj.protect(req, {
-    //   userId,
-    //   requested: 1, // Specify how many tokens to consume
-    // });
+    // Check rate limit
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // Specify how many tokens to consume
+    });
 
-    // if (decision.isDenied()) {
-    //   if (decision.reason.isRateLimit()) {
-    //     const { remaining, reset } = decision.reason;
-    //     console.error({
-    //       code: "RATE_LIMIT_EXCEEDED",
-    //       details: {
-    //         remaining,
-    //         resetInSeconds: reset,
-    //       },
-    //     });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
 
-    //     throw new Error("Too many requests. Please try again later.");
-    //   }
+        throw new Error("Too many requests. Please try again later.");
+      }
 
-    //   throw new Error("Request blocked");
-    // }
+      throw new Error("Request blocked");
+    }
 
-    // const user = await db.user.findUnique({
-    //   where: { clerkUserId: userId },
-    // });
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
 
     if (!user) {
       throw new Error("User not found");
